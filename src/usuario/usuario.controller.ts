d12271b1178ee6ea9addc18487994f44e85b8ee2
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2022-05-05 20:52:12 -0300

    feat: criacao de busca de usuario pelo nome

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Usuario } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
 
 @Controller('users')
 export class UsuarioController {
   constructor(private usuarioService: UsuarioService) {}
+
+  @Get(':nome')
+  public buscarNomeUsuario(@Param('nome') nome: string): Usuario {
+    const usuario = this.usuarioService.buscaNomeUsuario(nome);
+    return usuario;
+  }
 
   @Post()
   public cria(@Body() usuario: Usuario): Usuario {
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -3,7 +3,13 @@ import { Usuario } from './usuario.entity';
 
 @Injectable()
 export class UsuarioService {
-  private usuarios = [];
+  private usuarios: Array<Usuario>;
+
+  public buscaNomeUsuario(nome: string): Usuario {
+    const usuario = this.usuarios.find((usuario) => usuario.nome === nome);
+
+    return usuario;
+  }
 
   public cria(usuario: Usuario): Usuario {
     this.usuarios.push(usuario);
