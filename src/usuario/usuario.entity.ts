e229b6f46844e72a4f31193c2ad7e83276067aea
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2022-05-05 22:20:09 -0300

    feat: adicionado validacao de campos com ValidationPipe

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,8 +1,29 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class Usuario {
   id: number;
+
+  @IsNotEmpty({
+    message: 'nome é obrigatório',
+  })
+  @IsString({
+    message: 'nome precisa ser uma string',
+  })
   nome: string;
+
+  @IsEmail({
+    message: 'email invalido',
+  })
   email: string;
+
+  @IsNotEmpty({
+    message: 'senha é obrigatório',
+  })
   senha: string;
+
+  @IsNotEmpty({
+    message: 'nsomeCompleto é obrigatório',
+  })
   nomeCompleto: string;
   dataDeEntrada: Date;
 }
